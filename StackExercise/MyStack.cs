f42ea86f0c207b2b14e0eea3cc7ff872404f2cec
Rayyan-Asia <RayyanTawfieg@Gmail.com>
2023-01-29 13:30:46 +0200

    Complete

---
+++ StackExercise/MyStack.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExercise
+{
+    public class MyStack<T>
+    {
+        public List<T> Data;
+        public int Counter { get; set; }
+        public MyStack()
+        {
+            Data = new List<T>();
+            Counter = 0;
+        }
+        public void Push(T item)
+        {
+            Data.Add(item);
+            Counter++;
+        }
+        public T Pop()
+        {
+            if (Counter != 0)
+            {
+                T item = Data[Counter - 1];
+                Data.RemoveAt(Counter - 1);
+                Counter--;
+                return item;
+            }
+            else
+            {
+                throw new Exception("No Objects left in the stack");
+            }
+        }
+        public void Clear()
+        {
+            Counter = 0;
+            Data.Clear();
+        }
+
+        public T Peak()
+        {
+            if (Counter != 0)
+            {
+                T item = Data[Counter - 1];
+                return item;
+            }
+            else
+            {
+                throw new Exception("No Objects in the Stack");
+            }
+        }
+
+        public void Print()
+        {
+            if (Data.Count == 0) 
+            {
+                Console.WriteLine("No Objects in the Stack!");
+                return;
+            }
+            foreach (T item in Data)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        
+
+
+    }
+}
---
+++ StackExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace StackExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<string> stack = new MyStack<string>();
+            stack.Push("Rayyan");
+            stack.Push("is");
+            stack.Push("The");
+            stack.Push("Goat");
+            Console.WriteLine(stack.Peak());
+            stack.Print();
+            Console.WriteLine(stack.Pop());
+            stack.Clear();
+            stack.Print();
+
+            Console.ReadLine();
+
+        }
+    }
+}
